4d7978022987dce035a0080522c38b83d2074cbf
WheelFlower <kasternclaire@gmail.com>
2026-03-23 21:37:32 +0000

    Create dataframe and prepare features to test

---
+++ regression/multiple-linear-regression/mlr-basic.py
@@ -0,0 +1,21 @@
+import pandas as pd
+from sklearn.datasets import load_diabetes
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LinearRegression
+
+# get dataset as dataframe
+dataset = load_diabetes(as_frame=True, scaled=False)
+df = pd.DataFrame(dataset.data, columns=dataset.feature_names)
+df['progression'] = dataset.target
+
+# remove categorical data and clean
+df = df.drop(columns='sex')
+df.dropna()
+
+# find relationship between the features and the target variable
+f1 = ['age', 'bmi', 'bp', 's1', 's2', 's3', 's4', 's5', 's6'] # all possible features
+f2 = ['s1', 's2', 's3', 's4', 's5', 's6'] # only serum measurements
+f3 =  ['age', 's1', 's2', 's3', 's4', 's5', 's6'] # age and serum measurements
+f4 =  ['bmi', 's1', 's2', 's3', 's4', 's5', 's6'] # bmi and serum measurements
+f5 =  ['bp', 's1', 's2', 's3', 's4', 's5', 's6'] # bp and serum measurements
