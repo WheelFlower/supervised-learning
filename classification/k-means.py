a601b1c7d8de9e688645ba746d57a241c0adb783
WheelFlower <kasternclaire@gmail.com>
2026-03-30 20:19:04 -0500

    feat: scale and split dataset
    Added feature scaling and train-test split for k-means.

--- classification/k-means.py
+++ classification/k-means.py
@@ -14,3 +14,12 @@ df['species'] = iris.target
 
 # clean potential na
 df.dropna()
+
+#scale feature data
+X_raw = df.drop(columns=['species'])
+scaler = StandardScaler()
+X = scaler.fit_transform(X_raw)
+
+# split train and test
+y = df['species']
+X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.2)
