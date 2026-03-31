7d062b27c8bf4dc6911cc1e1c3c4a430460e055d
WheelFlower <kasternclaire@gmail.com>
2026-03-30 20:09:05 -0500

    Load Iris dataset and create DataFrame
    Added code to load the Iris dataset and create a DataFrame.

--- classification/k-means.py
+++ classification/k-means.py
@@ -4,3 +4,13 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.datasets import load_iris
+
+# obtain dataset
+iris = load_iris()
+
+# create dataframe
+df = pd.DataFrame(iris.data, columns = iris.feature_names)
+df['species'] = iris.target
+
+# clean potential na
+df.dropna()
