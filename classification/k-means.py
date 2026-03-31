e6b2c2d40ce2595f789c8cc363481d8e8c14d02f
WheelFlower <kasternclaire@gmail.com>
2026-03-30 20:38:03 -0500

    feat: compute and plot potential k values
    Added cross-validation to evaluate KNN model performance for different k-values and visualized the results using seaborn.

--- classification/k-means.py
+++ classification/k-means.py
@@ -1,9 +1,11 @@
 import pandas as pd
 import numpy as np
 from sklearn.preprocessing import StandardScaler
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, cross_val_score
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.datasets import load_iris
+import seaborn as sns
+import matplotlib.py as plt
 
 # obtain dataset
 iris = load_iris()
@@ -23,3 +25,17 @@ X = scaler.fit_transform(X_raw)
 # split train and test
 y = df['species']
 X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.2)
+
+# compute k-values
+k_values = [i for i in range(1,50)]
+scores = []
+for k in k_values:
+  knn = KNeighborsClassifier(n_neighbors=k)
+  score = cross_val_score(knn,X,y,cv=5)
+  scores.append(np.mean(score))
+
+# graph possible k-values
+sns.lineplot(x=k_values,y=scores,marker='o')
+plt.xlabel('K-Values')
+plt.ylabel('Accuracy Scores')
+plt.show()
