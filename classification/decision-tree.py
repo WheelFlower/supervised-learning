b840d21506ce71b2b4619b41d7cba9ea74edcdcb
WheelFlower <kasternclaire@gmail.com>
2026-04-14 16:09:36 +0000

    feat: created prediction method and updated decision tree png

--- classification/decision-tree.py
+++ classification/decision-tree.py
@@ -4,6 +4,7 @@ from sklearn import tree
 from sklearn.metrics import accuracy_score, confusion_matrix
 from sklearn.datasets import load_iris
 import matplotlib.pyplot as plt
+import numpy as np
 
 # obtain dataset
 iris = load_iris()
@@ -15,6 +16,7 @@ df['species'] = iris.target
 # clean potential na
 df.dropna()
 
+print(df)
 # split train and test data
 X = df.drop(columns=['species'])
 y = df.species
@@ -33,6 +35,25 @@ print('Confusion matrix:')
 print(confusion_matrix(y_test,y_predicts))
 
 fig = plt.figure(figsize=(79,90))
-tree.plot_tree(dectree, feature_names=X.columns.tolist(), fontsize=10)
+tree.plot_tree(dectree, feature_names=X.columns.tolist(), fontsize=20)
 plt.show()
 fig.savefig('decisiontree1.png')
+
+# predict new values
+def predict(values):
+    arr = np.array([values])
+    new_iris = pd.DataFrame(arr, columns = iris.feature_names)
+    prediction = dectree.predict(new_iris)
+    if prediction == 0:
+        species = "setosa"
+    elif prediction == 1:
+        species = "versicolor"
+    else:
+        species = "virginica"
+    return species
+
+iris1 = [5.6, 3.2, 4.1, 0.9]
+prediction1 = predict(iris1)
+print('Example iris:')
+print(iris1)
+print('Predicted to be:', prediction1)
