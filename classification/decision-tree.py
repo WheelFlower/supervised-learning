466ca6e428cee58718eb9f06ee585fc94e6baaea
WheelFlower <kasternclaire@gmail.com>
2026-04-13 21:13:00 -0500

    Import load_iris dataset in decision-tree.py

--- classification/decision-tree.py
+++ classification/decision-tree.py
@@ -2,4 +2,5 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn import tree
 from sklearn.metrics import accuracy_score, confusion_matrix
+from sklearn.datasets import load_iris
 import matplotlib.pyplot as plt
