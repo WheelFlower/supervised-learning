9de93ce2160a09f40d03422d07a1513bdd9c2a3b
WheelFlower <kasternclaire@gmail.com>
2026-04-13 21:16:51 -0500

    feat: prepare data for decision tree model
    Added data loading, cleaning, and splitting for decision tree.

--- classification/decision-tree.py
+++ classification/decision-tree.py
@@ -4,3 +4,19 @@ from sklearn import tree
 from sklearn.metrics import accuracy_score, confusion_matrix
 from sklearn.datasets import load_iris
 import matplotlib.pyplot as plt
+
+# obtain dataset
+iris = load_iris()
+
+# create dataframe
+df = pd.DataFrame(iris.data, columns = iris.feature_names)
+df['species'] = iris.target
+
+# clean potential na
+df.dropna()
+
+# split train and test data
+X = df.drop(columns=['species'])
+y = df.species
+
+X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.2)
