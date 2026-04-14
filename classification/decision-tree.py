f030d0643f302780be4395f4f09aa33032d8886e
WheelFlower <kasternclaire@gmail.com>
2026-04-13 21:11:43 -0500

    Add imports for decision tree implementation

---
+++ classification/decision-tree.py
@@ -0,0 +1,5 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn import tree
+from sklearn.metrics import accuracy_score, confusion_matrix
+import matplotlib.pyplot as plt
